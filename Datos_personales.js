eb629ad061db8601eeab8bf9d1f53f9fc4c7588c
sneider mendoza <sneider.mendoza@ludycom.com>
2022-09-21 17:23:13 -0500

    comienzo del curso

---
+++ Datos_personales.js
@@ -0,0 +1,10 @@
+const nombre = "Sneider Mendoza Suarez";
+const edad = 31;
+const desarrollador = true;
+const fecha_nacimiento = new Date("06 03 1991");
+let libro_favorito = {
+    titulo : "La culpa es de la vaca",
+    autor :"LOPERA GUTIÉRREZ, JAIME y BERNAL TRUJILLO, MARTA INÉS", 
+    fecha : 2000,
+    url:" https://quelibroleo.com/la-culpa-es-de-la-vaca", 
+}
